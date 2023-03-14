beb8a974e60a1dbbb41a90983fba6e0e27c17d06
mcct <mcct@privat.dk>
2023-03-14 17:05:29 +0100

    Færdig klar til aflevering.
    Viser en joker hvis bruger taster andet end et tal 1-52.  F.eks a,b,c, 53 eller lign.

--- Kortspil/MainWindow.xaml.cs
+++ Kortspil/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -18,23 +19,113 @@ namespace Kortspil
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int kortnummer = Convert.ToInt32(Kort.Text);
-            string filnavn = FindBillede(kortnummer);
-            string url = $"/Billeder/{filnavn}";
-            Uri uri = new (url, UriKind.Relative);
-            BitmapImage image = new(uri);
-           
-            Billede.Source = image;
+
+            // declare vars here, so I don't have to declare them twice in if else
+            int kortnummer;
+            string filnavn;
+            string url;
+            Uri uri;
             
+            // Tjek om input er et tal
+            if (Kort.Text.All(char.IsDigit))
+            {
+                kortnummer = Convert.ToInt32(Kort.Text);
+                filnavn = FindBillede(kortnummer);
+                url = $"/Billeder/{filnavn}";
+                uri = new(url, UriKind.Relative);
+                BitmapImage image = new(uri);
+                Billede.Source = image;
+
+                //    int kortnummer = Convert.ToInt32(Kort.Text);
+                //    string filnavn = FindBillede(kortnummer);
+                //    string url = $"/Billeder/{filnavn}";
+                //    Uri uri = new(url, UriKind.Relative);
+                //    BitmapImage image = new(uri);
+                //    Billede.Source = image;
+            }
+            else
+            {
+                // kortnummer = Convert.ToInt32(Kort.Text);
+                filnavn = "jokerOriginal.jpg";
+                url = $"/Billeder/{filnavn}";
+                uri = new(url, UriKind.Relative);
+                BitmapImage image = new(uri);
+                Billede.Source = image;
+
+                // show NA.jpg here
+                //string filnavn = "jokerOriginal.jpg";
+                //string url = $"/Billeder/{filnavn}";
+                //Uri uri = new(url, UriKind.Relative);
+                //BitmapImage image = new(uri);
+                //Billede.Source = image;
+            }
+
+
+            /////////////////////////////////////////
+            // Her er original code
+            /////////////////////////////////////////
+            //int kortnummer = Convert.ToInt32(Kort.Text);
+            //string filnavn = FindBillede(kortnummer);
+            //string url = $"/Billeder/{filnavn}";
+            //Uri uri = new(url, UriKind.Relative);
+            //BitmapImage image = new(uri);
+            //Billede.Source = image;
+
         }
 
         private string FindBillede(int kortnummer)
         {
-            string resultat = "2-Spar.jpg";
+            if (kortnummer <= 0 || kortnummer > 52 ) 
+            {
+                return "jokerOriginal.jpg";           
+            }
+           
+            int kortVærdi = (kortnummer - 1) % 13 + 1;  // Beregn kortets rang
+            int kulør = (kortnummer - 1) / 13 + 1;  // Beregn kortets kulør
 
-            // Skriv din løsning her...
+            // Konverter kulør-nummeret til en string
+            string kulørFarve = "";          
+            if(kulør == 1)
+            {
+                kulørFarve = "spar";
+            }
+            else if(kulør == 2)
+            {
+                kulørFarve = "ruder";
+            }
+            else if (kulør == 3)
+            {
+                kulørFarve = "klør";
+            }
+            else if (kulør == 4)
+            {
+                kulørFarve = "hjerter";
+            }
 
-            return resultat;
+            // Konverter rangen til en string
+            string rankNavn = "";
+            switch (kortVærdi)
+            {
+                case 1:
+                    rankNavn = "Es";
+                    break;
+                case 11:
+                    rankNavn = "Knægt";
+                    break;
+                case 12:
+                    rankNavn = "Dame";
+                    break;
+                case 13:
+                    rankNavn = "Konge";
+                    break;
+                default:
+                    rankNavn = kortVærdi.ToString();
+                    break;
+            }
+
+            // Sammensæt navnet og returner 
+            return $"{rankNavn}-{kulørFarve}.jpg";           
+
         }
     }
 }
